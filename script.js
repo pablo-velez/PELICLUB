61371259d81382e7519a8eb6f673eff9c421c576
pablo-velez <pablovelez999@gmail.com>
2025-10-05 23:19:42 -0300

    Update script.js
    Actualizacion de codigo

--- script.js
+++ script.js
@@ -1,58 +1,31 @@
-let loadMoreBtn1 = document.querySelector('#load-more-1');
-let currentItem1 = 4;
+const loadMoreButtons = document.querySelectorAll('.load-more');
 
-loadMoreBtn1.onclick = () => {
-    let boxes = [...document.querySelectorAll(
-        '.box-container-1 .box-1'
-    )];
-    for(var i = currentItem1; i < currentItem1 + 4; i++) {
-        boxes[i].style.display = 'inline-block';
- }
-    currentItem1 += 4;
-    if(currentItem1 >= boxes.length) {
-        loadMoreBtn1.style.display = 'none'
-    }
+loadMoreButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const boxContainer = button.previousElementSibling; 
+        const hiddenBoxes = boxContainer.querySelectorAll('.box[style*="display: none"], .box:not([style])'); // las ocultas
 
-}
+        let count = 0;
+        hiddenBoxes.forEach((box) => {
+            if (count < 4) { 
+                box.style.display = 'block';
+                count++;
+            }
+        });
 
-let loadMoreBtn2 = document.querySelector('#load-more-2');
-let currentItem2 = 4;
+        if (boxContainer.querySelectorAll('.box[style*="display: none"], .box:not([style])').length === 0) {
+            button.style.display = 'none';
+        }
+    });
+});
 
-loadMoreBtn2.onclick = () => {
-    let boxes = [...document.querySelectorAll(
-        '.box-container-2 .box-2'
-    )];
-    for(var i = currentItem2; i < currentItem2 + 4; i++) {
-        boxes[i].style.display = 'inline-block';
- }
-    currentItem2 += 4;
-    if(currentItem2 >= boxes.length) {
-        loadMoreBtn2.style.display = 'none'
-    }
-    
-}
-
-let loadMoreBtn3 = document.querySelector('#load-more-3');
-let currentItem3 = 4;
-
-loadMoreBtn3.onclick = () => {
-    let boxes = [...document.querySelectorAll(
-        '.box-container-3 .box-3'
-    )];
-    for(var i = currentItem3; i < currentItem3 + 4; i++) {
-        boxes[i].style.display = 'inline-block';
- }
-    currentItem3 += 4;
-    if(currentItem3 >= boxes.length) {
-        loadMoreBtn3.style.display = 'none'
-    }
-    
-}
-
-const user = JSON.parse(localStorage.getItem('login_succes')) || false
-if(!user){
-    window.location.href = 'login.html'
-}
+document.querySelectorAll('.box-container').forEach((container) => {
+    const boxes = container.querySelectorAll('.box');
+    boxes.forEach((box, index) => {
+        if (index < 4) box.style.display = 'block';
+        else box.style.display = 'none';
+    });
+});
 
 const logout = document.querySelector('#logout')
 
